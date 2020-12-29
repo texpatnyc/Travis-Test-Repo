22f710f1e594f51b0befa77fc579d152d3f84dc9
Kyle Combs <combskyle@gmail.com>
2020-12-29 14:24:45 -0600

    fixed test

--- success.js
+++ success.js
@@ -2,7 +2,7 @@ const figlet = require('figlet');
 
 // change the first argument of the call to figlet to have it print on success
 figlet.text(
-  'Cohort 23 is the coolest!!!',
+  'Cohort 22 is the coolest!!!',
   { horizontalLayout: 'full' },
   (err, data) => {
     if (err) {
